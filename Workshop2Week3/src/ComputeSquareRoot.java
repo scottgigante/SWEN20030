f7c396b3665a92dd00197e37bf83eb102b7a889c
Scott Gigante <scottgigante@gmail.com>
2014-09-27 09:07:22 +1000

    initial commit

---
+++ Workshop2Week3/src/ComputeSquareRoot.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.text.*;
+
+public class ComputeSquareRoot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// input n
+		Scanner s = new Scanner(System.in);
+		double n=0, r=0, guess=0, prev=0;
+		Boolean negative = false, initialised = false;
+		while (!initialised) {
+			System.out.println("Please enter an number:");
+			if (s.hasNextDouble()) {
+				initialised = true;
+				n = s.nextDouble();
+				guess = n/2;
+				prev = -guess;
+			} else {
+				System.out.println(s.next() + " is not a valid input.");
+			}
+		}
+		
+		// check for negatives
+		if (n < 0) {
+			n = -n;
+			guess = -guess;
+			prev = -prev;
+			negative = true;
+		}
+		
+		// find the square root
+		while (guess/prev < 0.99 || guess/prev > 1.01) {
+			r = n/guess;
+			prev = guess;
+			guess = (prev+r)/2;
+		}
+		
+		// print result
+		DecimalFormat f = new DecimalFormat("0.00");
+		System.out.println("The square root of " + (negative ? "-" : "") + n + " is " + f.format(guess) + (negative ? "i" : "") + "!");
+	}
+
+}
---
+++ Workshop2Week3/src/PredictHeight.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+
+public class PredictHeight {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		Boolean quit=false;
+		while (!quit) {
+			double mother=0, father=0, child=0;
+			Boolean init = false, male = false;
+			String input;
+			
+			// input father's height
+			while (!init && !quit) {
+				System.out.println("Please enter the father's height in inches, or \"q\" at any time to exit:");
+				if (s.hasNextDouble()) {
+					father = s.nextDouble();
+					if (father <= 0) {
+						System.out.println("Please enter a positive number.");
+					} else {
+						init = true;
+					}
+				} else if ((input = s.next()).matches("q")) {
+					quit = true;
+				} else {
+					System.out.println(input + " is not a valid input.");
+				}
+			}
+			
+			// input mother's height
+			init = false;
+			while (!init && !quit) {
+				System.out.println("Please enter the mother's height in inches:");
+				if (s.hasNextDouble()) {
+					mother = s.nextDouble();
+					if (mother <= 0) {
+						System.out.println("Please enter a positive number.");
+					} else {
+						init = true;
+					}
+				} else if ((input = s.next()).matches("q")) {
+					quit = true;
+				} else {
+					System.out.println(input + " is not a valid input.");
+				}
+			}
+			
+			// input gender
+			init = false;
+			while (!init && !quit) {
+				System.out.println("Please enter the child's gender: (M or F)");
+				input = s.next();
+				input = input.toLowerCase();
+				if (input.matches("m")) {
+					male = true;
+					init = true;
+				} else if (input.matches("q")) {
+						quit = true;
+				} else if (input.matches("f")) {
+					init = true;
+				} else {
+					System.out.println(input + " is not a valid input.");
+				}
+			}
+			
+			if (!quit) {
+				// calculate height
+				if (male) {
+					child = ((mother*13/12)+father)/2;
+				} else {
+					child = ((father*12/13)+mother)/2;
+				}
+				
+				// print result
+				System.out.printf("The child is predicted to be %.2f inches tall.\n", child);
+			}
+		}
+	}
+
+}
